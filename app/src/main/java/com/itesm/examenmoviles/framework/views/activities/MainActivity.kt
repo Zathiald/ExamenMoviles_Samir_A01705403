989aa0c9e0dc10ca2cfd0c5d4c1b105a022abbe4
Zathiald <A01705403@tec.mx>
2024-10-06 20:01:25 -0600

    Refactor: Agregar paquetes y modularizacion

--- app/src/main/java/com/itesm/examenmoviles/framework/views/activities/MainActivity.kt
+++ app/src/main/java/com/itesm/examenmoviles/framework/views/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.itesm.examenmoviles
+package com.itesm.examenmoviles.framework.views.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.itesm.examenmoviles.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
